f6777767265c29c14ab788e24d85cb4b8eec1bd3
kethan <dullakethan02@gmail.com>
2025-03-13 09:04:46 +0530

    sec commit

--- src/test.java
+++ src/test.java
@@ -3,5 +3,7 @@ import java.sql.SQLOutput;
 public class test {
     public static void main(String[]args){
         System.out.println("Hello World ");
+        System.out.println("Hi");
+
     }
 }
